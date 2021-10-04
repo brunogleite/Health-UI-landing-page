60091e7f503b3b4fb54c662d5fb75fedb0fe7911
brunogleite <brunoleite6o@gmail.com>
2021-10-04 14:12:45 +0100

    hover states on nav links and replacing imgs to svg

---
+++ app.js
@@ -0,0 +1,9 @@
+//Get nav-links
+var introLink = document.querySelector(".intro-link");
+var hightlightLink = document.querySelector(".highlight-link");
+var overviewLink = document.querySelector(".overview-link");
+var darkmodeLink = document.querySelector(".darkmode-link");
+
+introLink.addEventListener("click", (e)=>{
+    console.log(e)
+})
