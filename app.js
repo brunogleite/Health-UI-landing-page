6b2475236c2c0cec47101326f5161a1f6b79c0e3
brunogleite <brunoleite6o@gmail.com>
2021-10-05 01:50:21 +0100

    repairing cta homepage

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var navLinks = document.querySelectorAll(".nav-links li");
 
 
-
 navLinks.forEach(link => {
    link.addEventListener("click", ()=>{
         navLinks.forEach(list => list.classList.remove("active"))
