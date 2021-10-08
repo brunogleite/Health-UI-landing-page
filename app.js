7d4534eb74ba0044041e47f09df18baed3243b7c
brunogleite <brunoleite6o@gmail.com>
2021-10-08 16:20:01 +0100

    header drop done

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 //Get nav-links
 var navLinks = document.querySelectorAll(".nav-links li");
 
+//Menu burguer
+var burguerMenu = document.querySelector(".hamburguer");
+let menuOpen = false; 
+
+//function for burguer animation
+burguerMenu.addEventListener("click", ()=>{
+   if(!menuOpen){
+      burguerMenu.classList.add("open");
+      menuOpen = true; 
+      nav.style.display = "flex";
+      nav.style.top = 0;
+   } else{
+
+      burguerMenu.classList.remove("open")
+      menuOpen = false; 
+      nav.style.top = '-100%'; 
+   }
+});
+
+
+
+
+
+//get the modal
+var nav = document.querySelector(".nav-links")
 
 navLinks.forEach(link => {
    link.addEventListener("click", ()=>{
         navLinks.forEach(list => list.classList.remove("active"))
-        link.classList.add("active");  
+        link.classList.add("active"); 
+        menuOpen = false; 
+         nav.style.top = '-100%'; 
    })
-})
+});
+
+
+
+
